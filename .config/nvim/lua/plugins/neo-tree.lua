30b07669fbf9fa97798d2605f415c9169d5d7a22
Gabriel Machado <gabriel.galoma@gmail.com>
2025-01-08 18:37:09 -0300

    update: add nwes configs for lazy.nvim plugins

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,8 +5,146 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
+        {
+            "s1n7ax/nvim-window-picker",
+            version = "2.*",
+            config = function()
+                require("window-picker").setup({
+                    filter_rules = {
+                        include_current_win = false,
+                        autoselect_one = true,
+                        -- filter using buffer options
+                        bo = {
+                            -- if the file type is one of following, the window will be ignored
+                            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+                            -- if the buffer type is one of following, the window will be ignored
+                            buftype = { "terminal", "quickfix" },
+                        },
+                    },
+                })
+            end,
+        },
+    },
+    keys = {
+        { "<C-n>", "<cmd>Neotre filesystem reveal toggle<CR>", desc = "open neo-tree" },
     },
     config = function()
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+        -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+        vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+        require("neo-tree").setup({
+            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+            sort_case_insensitive = false, -- used when sorting files and directories in the tree
+            sort_function = nil, -- use a custom function for sorting files and directories in the tree
+            -- sort_function = function (a,b)
+            --       if a.type == b.type then
+            --           return a.path > b.path
+            --       else
+            --           return a.type > b.type
+            --       end
+            --   end , -- this sorts files and directories descendantly
+            default_component_configs = {
+                container = {
+                    enable_character_fade = true,
+                },
+                indent = {
+                    indent_size = 2,
+                    padding = 1, -- extra padding on left hand side
+                    -- indent guides
+                    with_markers = true,
+                    indent_marker = "*",
+                    last_indent_marker = "└",
+                    highlight = "NeoTreeIndentMarker",
+                    -- expander config, needed for nesting files
+                    with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+                icon = {
+                    folder_closed = "",
+                    folder_open = "",
+                    folder_empty = "󰜌",
+                    provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
+                        if node.type == "file" or node.type == "terminal" then
+                            local success, web_devicons = pcall(require, "nvim-web-devicons")
+                            local name = node.type == "terminal" and "terminal" or node.name
+                            if success then
+                                local devicon, hl = web_devicons.get_icon(name)
+                                icon.text = devicon or icon.text
+                                icon.highlight = hl or icon.highlight
+                            end
+                        end
+                    end,
+                    -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                    -- then these will never be used.
+                    default = "*",
+                    highlight = "NeoTreeFileIcon",
+                },
+                modified = {
+                    symbol = "[+]",
+                    highlight = "NeoTreeModified",
+                },
+                name = {
+                    trailing_slash = false,
+                    use_git_status_colors = true,
+                    highlight = "NeoTreeFileName",
+                },
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted = "✖", -- this can only be used in the git_status source
+                        renamed = "󰁕", -- this can only be used in the git_status source
+                        -- Status type
+                        untracked = "",
+                        ignored = "",
+                        unstaged = "󰄱",
+                        staged = "",
+                        conflict = "",
+                    },
+                },
+                -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
+                file_size = {
+                    enabled = false,
+                    width = 12, -- width of the column
+                    required_width = 64, -- min width of window required to show this column
+                },
+                type = {
+                    enabled = false,
+                    width = 10, -- width of the column
+                    required_width = 122, -- min width of window required to show this column
+                },
+                last_modified = {
+                    enabled = false,
+                    width = 20, -- width of the column
+                    required_width = 88, -- min width of window required to show this column
+                },
+                created = {
+                    enabled = false,
+                    width = 20, -- width of the column
+                    required_width = 110, -- min width of window required to show this column
+                },
+                symlink_target = {
+                    enabled = true,
+                },
+            },
+            window = {
+                position = "left",
+                width = 80,
+                mapping_options = {
+                    noremap = true,
+                    nowait = true,
+                },
+            },
+        })
     end,
 }
