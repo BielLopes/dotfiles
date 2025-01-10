068c88222fce560e1d2d8ca4f489f7a9fc90102c
Gabriel Machado <gabriel.galoma@gmail.com>
2025-01-10 18:57:15 -0300

    update: neo-tree ui update

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -59,7 +59,7 @@ return {
                     padding = 1, -- extra padding on left hand side
                     -- indent guides
                     with_markers = true,
-                    indent_marker = "",
+                    indent_marker = "",
                     last_indent_marker = "└",
                     highlight = "NeoTreeIndentMarker",
                     -- expander config, needed for nesting files
