ecb1cc4e9bfd72865228cfca913207106f02df55
Gabriel Machado <gabriel.galoma@gmail.com>
2025-01-12 15:00:28 -0300

    fix: comment unsuported dependency for eslint_d

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.diagnostics.eslint_d,
+                -- null_ls.builtins.diagnostics.eslint_d,
                 require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
                 null_ls.builtins.completion.spell,
             },
