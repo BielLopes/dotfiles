8367fcdd8cbf2f8a7877237c920de93efbf67d16
Gabriel Machado <gabriel.galoma@gmail.com>
2025-01-29 16:52:08 -0300

    add: global keymap to execute cargo test

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -20,14 +20,22 @@ return {
             -- Send the command to the default terminal
             vim.cmd("TermExec cmd='cargo run'")
         end)
+        -- Run cargo test on terminal
+        vim.keymap.set("n", "<leader>ct", function()
+            vim.cmd("ToggleTerm")
+            -- Send the command to the default terminal
+            vim.cmd("TermExec cmd='cargo test'")
+        end)
         -- Run Harlequin on custom terminal
-        local Terminal = require('toggleterm.terminal').Terminal
-        local sil_harlenquin  = Terminal:new({ cmd = "harlequin --adapter postgres --host localhost --port 5432 -U admin --password admin --dbname sil-versao-nova", hidden = false })
+        local Terminal = require("toggleterm.terminal").Terminal
+        local sil_harlenquin = Terminal:new({
+            cmd =
+            "harlequin --adapter postgres --host localhost --port 5432 -U admin --password admin --dbname sil-versao-nova",
+            hidden = false,
+        })
 
-
-        vim.keymap.set("n", "<leader>sh",
-            function()
-                sil_harlenquin:toggle()
-            end)
+        vim.keymap.set("n", "<leader>sh", function()
+            sil_harlenquin:toggle()
+        end)
     end,
 }
