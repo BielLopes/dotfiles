11af25e7f0b3765cda4d72a4a2ec25ff2b11943e
Gabriel Machado <gabriel.galoma@gmail.com>
2025-02-21 08:43:30 -0300

    fix: change rename keys to 'rn'

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -60,7 +60,7 @@ return {
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
